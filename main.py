e1e6a80a7e780d3043585b8c8b11fdb74dfcc5d7
patrickzhou1234 <patrickchang44543@gmail.com>
2023-02-11 11:39:07 -0800

    allthreelists

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
 
-list = {
+namelist = {
   
 }
+
+idlist = {
+
+}
+
+nameidlist = {
+
+}
+
+i=0
 
 app = Flask(__name__)
 
@@ -13,9 +23,13 @@ def index():
 
 @app.route('/', methods=['POST'])
 def renderresult():
+  global i
   name = request.form['name']
   score = request.form['score']
-  list[name]=score
-  return render_template('index.html', result=list)
+  namelist[name]=score
+  idlist[i]=score
+  nameidlist[name]=i
+  i+=1
+  return render_template('index.html', result=namelist, idlist=idlist)
 
 app.run(host='0.0.0.0', port=80)
